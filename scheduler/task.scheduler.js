59b38bcb7af7f2f06613f2bd872052d2498c025b
fradotech <fradotech.id@gmail.com>
2023-10-27 12:16:59 +0700

    feat: pasang scheduler per detik and bot tele

--- scheduler/task.scheduler.js
+++ scheduler/task.scheduler.js
@@ -1,6 +1,9 @@
+import TelegramBot from "node-telegram-bot-api";
 import Task from "../models/task.js";
 
 const minuteForSendReminder = 1;
+const token = "6882209271:AAHeOwFX_v6R5NDjvGeoGWZJ5gZLz9Lisiw";
+const chatBot = new TelegramBot(token, { polling: true });
 
 export const taskScheduler = {
   reminderDeadline: async () => {
